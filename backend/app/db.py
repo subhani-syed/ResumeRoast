a4759f2c111fc79e683632c35c72e4df5d16814a
Subhani-syed <subhani6352@gmail.com>
2026-02-23 10:04:33 +0530

    refactor: change db URL from SQLite to PostgreSQL

--- backend/app/db.py
+++ backend/app/db.py
@@ -7,8 +7,6 @@ DATABASE_URL = settings.DATABASE_URL
 class Base(DeclarativeBase):
     pass
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- backend/app/main.py
+++ backend/app/main.py
@@ -9,13 +9,13 @@ from app import models
 app = FastAPI()
 
 origins = [
-    "http://localhost",
-    "http://localhost:8080",
+    "http://localhost:3000",
+    "http://ui:3000",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
